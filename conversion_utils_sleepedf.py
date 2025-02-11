f0c061c1adbbb67c86465e1212ed9ecf46f3891e
Javier Garcia <jgciudad@outlook.com>
2025-02-11 17:09:31 +0100

    scripts adapted to sleepedf data

---
+++ conversion_utils_sleepedf.py
@@ -0,0 +1,174 @@
+import numpy as np
+import torch
+from torch.nn import functional as F
+from collections import defaultdict
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+@torch.inference_mode(True)
+def sample_latents(loader, subject_latents, task_latents, target_subject, target_task, n=2000, specific_task='all', specific_subject='all'):
+    if isinstance(specific_subject, int):
+        convert_task_latents = task_latents[(loader.subjects == specific_subject) & (loader.tasks == target_task)]
+    elif specific_subject == 'all':
+        convert_task_latents = task_latents[loader.tasks == target_task]
+    elif specific_subject == 'same':
+        convert_task_latents = task_latents[(loader.subjects == target_subject) & (loader.tasks == target_task)]
+    elif specific_subject == 'different':
+        convert_task_latents = task_latents[(loader.subjects != target_subject) & (loader.tasks == target_task)]
+    else:
+        raise ValueError('specific_subject must be one of [#subject_class_label#, all, same, different]')
+    if isinstance(specific_task, int):
+        convert_subject_latents = subject_latents[(loader.subjects == target_subject) & (loader.tasks == specific_task)]
+    elif specific_task == 'all':
+        convert_subject_latents = subject_latents[loader.subjects == target_subject]
+    elif specific_task == 'same':
+        convert_subject_latents = subject_latents[(loader.subjects == target_subject) & (loader.tasks == target_task)]
+    elif specific_task == 'different':
+        convert_subject_latents = subject_latents[(loader.subjects == target_subject) & (loader.tasks != target_task)]
+    else:
+        raise ValueError('specific_task must be one of [#task_class_label#, all, same, different]')
+    
+    if convert_task_latents.shape[0] != 0:
+        num_task_latents = convert_task_latents.shape[0]
+        if num_task_latents < n:
+            task_permute_idxs = np.random.randint(0, num_task_latents, size=n)
+        else:
+            task_permute_idxs = np.random.permutation(num_task_latents)[:n]
+        convert_task_latents = convert_task_latents[task_permute_idxs]
+    else:
+        print(f'convert_task_latents is empty with target_subject={target_subject}, target_task={target_task}, specific_subject={specific_subject}, specific_task={specific_task}.')
+        print("Ignoring it in reconstruction loss.")
+
+    if convert_subject_latents.shape[0] != 0:
+        num_subject_latents = convert_subject_latents.shape[0]
+        if num_subject_latents < n:
+            subject_permute_idxs = np.random.randint(0, num_subject_latents, size=n)
+        else:
+            subject_permute_idxs = np.random.permutation(num_subject_latents)[:n]
+        convert_subject_latents = convert_subject_latents[subject_permute_idxs]
+    else:
+        print(f'convert_subject_latents is empty with target_subject={target_subject}, target_task={target_task}, specific_subject={specific_subject}, specific_task={specific_task}.')
+        print("Ignoring it in reconstruction loss.")
+
+    return convert_subject_latents, convert_task_latents
+
+
+@torch.inference_mode(True)
+def reconstruct(model, convert_subject_latents, convert_task_latents, batch_size=2048):
+    num_latents = convert_subject_latents.shape[0]
+    num_batches = int(np.ceil(num_latents / batch_size))
+    convert_subject_latents = torch.unflatten(torch.tensor(convert_subject_latents, device=device), 1, (model.latent_dim, model.latent_seqs))
+    convert_task_latents = torch.unflatten(torch.tensor(convert_task_latents, device=device), 1, (model.latent_dim, model.latent_seqs))
+    reconstructions = []
+    for i in range(num_batches):
+        start_idx = i * batch_size
+        end_idx = (i+1) * batch_size
+        x_dict = {'s': convert_subject_latents[start_idx:end_idx], 't': convert_task_latents[start_idx:end_idx]}
+        x_dict = model.get_x_hat(x_dict)
+        reconstructions.append(x_dict['x_hat'])
+    return torch.cat(reconstructions, dim=0)
+
+@torch.inference_mode(True)
+def get_reconstructed_erps(model, loader, subject_latents, task_latents, t_spec, s_spec, target_subject, target_task1, n):
+    
+    convert_subject_latents, convert_task_latents = sample_latents(loader, subject_latents, task_latents, target_subject, target_task1, n=n, specific_task=t_spec, specific_subject=s_spec)
+    
+    if convert_task_latents.shape[0] != 0 and convert_subject_latents.shape[0] != 0:
+        reconstructions = reconstruct(model, convert_subject_latents, convert_task_latents)
+        reconstructions = reconstructions*loader.data_std + loader.data_mean
+        reconstructed_erp1 = reconstructions.mean(0)
+    else:
+        reconstructed_erp1 = None
+
+    return reconstructed_erp1
+
+@torch.inference_mode(True)
+def get_conversion_results(model, loader, subject_latents, task_latents, target_subject, target_task, channel, n):
+    real_erp1 = loader.data[(loader.subjects == target_subject) & (loader.tasks == target_task)]
+    real_erp1 = real_erp1.to(device) * loader.data_std + loader.data_mean
+    real_erp1 = real_erp1.mean(0)
+    recon_erp_ss1 = get_reconstructed_erps(model, loader, subject_latents, task_latents, 'same', 'same', target_subject, target_task, n)
+    recon_erp_sd1 = get_reconstructed_erps(model, loader, subject_latents, task_latents, 'same', 'different', target_subject, target_task, n)
+    recon_erp_ds1 = get_reconstructed_erps(model, loader, subject_latents, task_latents, 'different', 'same', target_subject, target_task, n)
+    recon_erp_dd1 = get_reconstructed_erps(model, loader, subject_latents, task_latents, 'different', 'different', target_subject, target_task, n)
+    
+    ss_mse = F.mse_loss(recon_erp_ss1[channel], real_erp1[channel]) if recon_erp_ss1 is not None else None
+    sd_mse = F.mse_loss(recon_erp_sd1[channel], real_erp1[channel]) if recon_erp_sd1 is not None else None
+    ds_mse = F.mse_loss(recon_erp_ds1[channel], real_erp1[channel]) if recon_erp_ds1 is not None else None
+    dd_mse = F.mse_loss(recon_erp_dd1[channel], real_erp1[channel]) if recon_erp_dd1 is not None else None
+
+    return ss_mse, sd_mse, ds_mse, dd_mse
+
+
+@torch.inference_mode(True)
+def get_full_conversion_results(model, test_loader, subject_latents, task_latents, N):
+    channel_names = [
+        'Fpz-Cz']
+    channel_to_idx = {c: i for i, c in enumerate(channel_names)}
+    task_to_channel = {1: 'Fpz-Cz', 2: 'Fpz-Cz', 3: 'Fpz-Cz', 4: 'Fpz-Cz', 5: 'Fpz-Cz'}
+    ss_mses, sd_mses, ds_mses, dd_mses = [], [], [], []
+    mse_results = {}
+    for target_subject in test_loader.unique_subjects:
+        for target_task in test_loader.unique_tasks:
+            channel = channel_to_idx[task_to_channel[target_task]]
+            ss_mse, sd_mse, ds_mse, dd_mse = get_conversion_results(model, test_loader, subject_latents, task_latents, target_subject, target_task, channel, N)
+            paradigm_name = test_loader.task_to_label[target_task]
+            mse_results[f'MSE/test/{paradigm_name}/{target_subject}/ss'] = ss_mse
+            mse_results[f'MSE/test/{paradigm_name}/{target_subject}/sd'] = sd_mse
+            mse_results[f'MSE/test/{paradigm_name}/{target_subject}/ds'] = ds_mse
+            mse_results[f'MSE/test/{paradigm_name}/{target_subject}/dd'] = dd_mse
+            ss_mses.append(ss_mse)
+            sd_mses.append(sd_mse)
+            ds_mses.append(ds_mse)
+            dd_mses.append(dd_mse)
+    #Calculate per subject mean
+    for target_subject in test_loader.unique_subjects:
+        ss_results, sd_results, ds_results, dd_results = [], [], [], []
+        for target_task in test_loader.unique_tasks:
+            paradigm_name = test_loader.task_to_label[target_task]
+            ss_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/ss'])
+            sd_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/sd'])
+            ds_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/ds'])
+            dd_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/dd'])
+            
+        ss_results = [x for x in ss_results if x is not None]
+        sd_results = [x for x in sd_results if x is not None]
+        ds_results = [x for x in ds_results if x is not None]
+        dd_results = [x for x in dd_results if x is not None]
+        
+        mse_results[f'MSE/test/mean/{target_subject}/ss'] = torch.mean(torch.stack(ss_results))
+        mse_results[f'MSE/test/mean/{target_subject}/sd'] = torch.mean(torch.stack(sd_results))
+        mse_results[f'MSE/test/mean/{target_subject}/ds'] = torch.mean(torch.stack(ds_results))
+        mse_results[f'MSE/test/mean/{target_subject}/dd'] = torch.mean(torch.stack(dd_results))
+    #Calculate per paradigm mean
+    for target_task in test_loader.unique_tasks:
+        paradigm_name = test_loader.task_to_label[target_task]
+        ss_results, sd_results, ds_results, dd_results = [], [], [], []
+        for target_subject in test_loader.unique_subjects:
+            ss_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/ss'])
+            sd_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/sd'])
+            ds_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/ds'])
+            dd_results.append(mse_results[f'MSE/test/{paradigm_name}/{target_subject}/dd'])
+        
+        ss_results = [x for x in ss_results if x is not None]
+        sd_results = [x for x in sd_results if x is not None]
+        ds_results = [x for x in ds_results if x is not None]
+        dd_results = [x for x in dd_results if x is not None]
+        
+        mse_results[f'MSE/test/mean/{paradigm_name}/ss'] = torch.mean(torch.stack(ss_results))
+        mse_results[f'MSE/test/mean/{paradigm_name}/sd'] = torch.mean(torch.stack(sd_results))
+        mse_results[f'MSE/test/mean/{paradigm_name}/ds'] = torch.mean(torch.stack(ds_results))
+        mse_results[f'MSE/test/mean/{paradigm_name}/dd'] = torch.mean(torch.stack(dd_results))
+    
+    ss_mses = [x for x in ss_mses if x is not None]
+    sd_mses = [x for x in sd_mses if x is not None]
+    ds_mses = [x for x in ds_mses if x is not None]
+    dd_mses = [x for x in dd_mses if x is not None]
+
+    #Calculate overall mean
+    mse_results['MSE/test/mean/ss'] = torch.mean(torch.stack(ss_mses))
+    mse_results['MSE/test/mean/sd'] = torch.mean(torch.stack(sd_mses))
+    mse_results['MSE/test/mean/ds'] = torch.mean(torch.stack(ds_mses))
+    mse_results['MSE/test/mean/dd'] = torch.mean(torch.stack(dd_mses))
+    
+    return mse_results
---
+++ reevaluate_sleepedfx.py
@@ -0,0 +1,92 @@
+import sys
+import wandb
+from split_model import SplitLatentModel
+import torch
+import numpy as np
+from utils_sleepedfx import get_results, get_split_latents, split_do_tsne, CustomLoader
+from tqdm import tqdm
+
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ids', type=str, default='')
+parser.add_argument('--data_line', type=str, default='simple')
+parser.add_argument('--data_dir', type=str, default='./')
+parser.add_argument('--model_save_dir', type=str, default='./')
+parser.add_argument('--save_dir', type=str, default='./')
+parser.add_argument('--wandb_directory', type=str, default='converting-erps/')
+
+args, unknown = parser.parse_known_args()
+
+if __name__ == '__main__':
+    ids = args.ids.split(',')
+    api = wandb.Api()
+    runs = [api.run(args.wandb_directory + id) for id in ids]
+    with torch.no_grad():
+        fold=0
+        data_dict = torch.load(args.data_dir+f"{args.data_line}_data.pt")
+        data_dict["data"] = (data_dict["data"] - data_dict["fold_info"][fold]["mean"]) / data_dict["fold_info"][fold]["std"]
+        loader = CustomLoader(data_dict, fold=fold, split='train')
+        test_loader = CustomLoader(data_dict, fold=fold, split='test')
+        del data_dict
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    for run in tqdm(runs):
+        SEED = run.config['seed']   
+        torch.manual_seed(SEED)
+        np.random.seed(SEED)
+        IN_CHANNELS = run.config['in_channels']
+        NUM_LAYERS = run.config['num_layers']
+        KERNEL_SIZE = 4
+        CHANNELS = run.config['channels']
+        LATENT_DIM = run.config['latent_dim']
+        RECON_TYPE = run.config['recon_type']
+        
+        model = SplitLatentModel(IN_CHANNELS, CHANNELS, LATENT_DIM, NUM_LAYERS, KERNEL_SIZE, recon_type=RECON_TYPE, time_resolution=240)
+        
+        losses = run.config['losses']
+        
+        model.set_losses(
+            batch_size=run.config['batch_size'],
+            losses=losses,
+            loader=loader,
+        )
+        state_dict = torch.load(f"{args.model_save_dir}{run.id}-{run.name}.pt", map_location=device)
+        model.load_state_dict(state_dict)
+        data_out = {}
+        model = model.to(device)
+        model.eval()
+        with torch.inference_mode(True):
+            model.loader = loader
+            print(f'Getting train latents {run.name}', file=sys.stdout, flush=True)
+            subject_latents, task_latents, subjects, tasks, runs, losses = get_split_latents(model, loader, loader.get_dataloader(batch_size=model.batch_size, random_sample=False))
+            print(f'Getting train tsne {run.name}', file=sys.stdout, flush=True)
+            start_time = time.time()
+            # indices = np.random.choice(len(subject_latents), size=int(len(subject_latents) * 0.1), replace=False) # TODO remove this line
+            # subject_latents = subject_latents[indices]
+            # task_latents = task_latents[indices]
+            subject_tsne, task_tsne = split_do_tsne(subject_latents, task_latents)
+            end_time = time.time()
+            print(f"Time taken to run train tsne: {end_time - start_time} seconds", file=sys.stdout, flush=True)
+            data_out['subject_tsne'] = subject_tsne
+            data_out['task_tsne'] = task_tsne
+            model.loader = test_loader
+            print(f'Getting test latents {run.name}', file=sys.stdout, flush=True)
+            subject_latents, task_latents, subjects, tasks, runs, losses = get_split_latents(model, test_loader, test_loader.get_dataloader(batch_size=model.batch_size, random_sample=False))
+            print(f'Getting test tsne {run.name}', file=sys.stdout, flush=True)
+            start_time = time.time()
+            subject_tsne, task_tsne = split_do_tsne(subject_latents, task_latents)
+            end_time = time.time()
+            print(f"Time taken to run test tsne: {end_time - start_time} seconds", file=sys.stdout, flush=True)
+            data_out['subject_tsne_test'] = subject_tsne
+            data_out['task_tsne_test'] = task_tsne
+            print(f'Getting test results {run.name}', file=sys.stdout, flush=True)
+            test_results = get_results(subject_latents, task_latents, subjects, tasks, split=test_loader.split, off_class_accuracy=False)
+            subject_cm = test_results['XGB/' + test_loader.split + '/' + 'subject/cm']
+            task_cm = test_results['XGB/' + test_loader.split + '/' + 'task/cm']
+            data_out['subject_cm'] = subject_cm
+            data_out['task_cm'] = task_cm
+            data_out['test_results'] = test_results
+            torch.save(data_out, f"{args.save_dir}/{run.name}_result_data.pt")
